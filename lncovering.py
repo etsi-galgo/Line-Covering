8684e980d8297b7b225ff0ecb834dc90e82fc54c
Alina Kasiuk <jupiterianw@gmail.com>
2023-03-01 13:28:53 +0100

    Greegy algorithm ending on the end of the segment
    Also added finishing and beginning right on the greedy point

---
+++ lncovering.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 10:34:12 2023
+
+@author: Alina
+"""
+
+import numpy as np
+import random
+import greedy
+
+def LineGenerate(lenght, m):
+    """
+    Creating a line with m random segments
+    """
+    line = np.zeros(lenght,)
+    # random start and end of the segments
+    ab = np.array(sorted(random.sample(range(0, lenght-1), m*2))) 
+    ab = ab.reshape((m,2)) # start and end of the segments coordinates
+    # positive points coordinates
+    ones = np.arange(ab[0,0],ab[0,1])
+    for i in range(1,m):
+        ones = np.concatenate((ones, np.arange(ab[i,0],ab[i,1])))
+    line[ones] = 1  # put the generated segments on the line
+    return line, ones
+
+
+def main():
+    lenght = 100 # line length
+    m = 7 # number of segments
+    base = np.array((50,50)) # base station coordinates
+    
+    # Generate a line to cover
+    line, ones = LineGenerate(lenght, m)
+    L = 142
+    n, totalL = greedy.GreedySE(base,line, ones,L)
+    print('Total number of tours:', n)
+    print('Total lenght:', totalL)
+
+
+if __name__ == "__main__":
+    main()
