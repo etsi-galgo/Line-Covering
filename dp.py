e762fcc3fd718224dd7c18e44dea7490f585a523
Alina Kasiuk <jupiterianw@gmail.com>
2023-03-26 20:10:23 +0200

    Recursion

--- dp.py
+++ dp.py
@@ -47,5 +47,46 @@ def AllCandidates(base, L, ab):
         ab_cut = ab_cut[:ab_cut.shape[0]-1]
     return c, c_ass
 
+def SegmentNumber(c, ab):
+    for i in range(0,ab.shape[0]):
+        if (c >= ab[i,0]) & (c <= ab[i,1]):
+            return i 
 
+def FindAssociated(c, ab, base, L):
+    # distance from the farthest point to the base
+    b = math.sqrt(c**2+base[1]**2)
+    # distance from the farthest point to greedy
+    dist = (L**2-2*L*b)/(2*(L-b-c))
+    c_ass = c-dist
+    for i in range(0,ab.shape[0]):
+        if (c_ass > ab[i-1,1]) & (c_ass < ab[i,0]):
+            c_ass = ab[i,0]
+        if (c_ass < ab[0,0]):
+            c_ass = ab[0,0]  
+    return c_ass
+
+def Recursion(c, ab, base, L):
+    c_ass = FindAssociated(c, ab, base, L)
+    j = np.arange(SegmentNumber(c_ass, ab), SegmentNumber(c, ab)+1)
+    print(c)    
+    print(c_ass)    
+    if c_ass == ab[0,0]:
+        return (c - ab[0,0])
+    
+    elif c_ass in ab[1:,0]:
+        func = np.empty(0)
+        for i in j:
+            func = np.append(func, Recursion(ab[i-1,1], ab, base, L))    
+        func1 = (c - ab[j,0]) + func
+        return min(func1)
+    
+    else: 
+        func = np.empty(0)
+        for i in j:
+            func = np.append(func, Recursion(ab[i-1,1], ab, base, L))    
+        func1 = (c - ab[j,0]) + func
+        
+        func2 = L + Recursion(c_ass, ab, base, L)
+        
+        return min(func2, min(func1))
 
